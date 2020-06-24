02692f857a0b3b2f211f0b9e2c064a250f4a16ea
MarvinDlg <marvin-dilg@web.de>
2020-06-24 17:06:16 +0200

    add WelcomeScreen.js

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,31 +1,64 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
+import AppButton from '../components/Button/AppButton';
+import colors from '../config/colors';
 
 function WelcomeScreen(props) {
     return (
-        <View 
-        style={styles.background}>
-
+        <ImageBackground 
+        blurRadius={5}
+        style={styles.background}
+        >
+        
+        <View style={styles.logoContainer}>
         <Image 
-        style={styles.logo} 
+        style={styles.logo}
         source={require('../assets/logo.png')} />
+        <Text style={styles.tagline}>GET YOUR LIFE TOGETHER</Text>
+        </View>
+
+        <View style={styles.buttonsContainer}>
+        <AppButton title="Login" color="third" />
+        <AppButton title="Register" color="third"/>
+        
 
         </View>
+        
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     background: {
         flex: 1, 
-        backgroundColor: '#000', 
-        justifyContent: 'center', 
+        justifyContent: "flex-end", 
         alignItems: 'center', 
+        backgroundColor: '#000', 
     }, 
+    buttonsContainer: {
+        padding: 20,
+        width: '100%', 
+    },
     logo: {
-        width: 100, 
-        height: 100, 
-    }, 
-      
+       width: 150,
+       height: 150, 
+       
+    },
+    logoContainer: {
+        position: 'absolute',
+       top: 170, 
+       alignItems: 'center', 
+    },
+    tagline: {
+    fontSize: 20, 
+    fontWeight: "600",
+    paddingVertical: 0,
+    color: colors.third, 
+    
+
+
+    }
+    
 });
 
 export default WelcomeScreen;
