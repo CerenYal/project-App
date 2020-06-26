4404dec86bad6736c3ae6c18fead7ce899a30429
MarvinDlg <marvin-dilg@web.de>
2020-06-26 15:53:20 +0200

    merge with branch marvin

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
   return (
-      <WelcomeScreen />
+    // <WelcomeScreen />
+      <LoginScreen />
   );
 }
 
